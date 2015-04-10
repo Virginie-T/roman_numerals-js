472c2b3db81ac8c16966aa97b5fbd17cbcdbd77d
virginie <virginietrubiano@gmail.com>
2015-04-10 10:13:49 -0700

    basic structure of the project + 3 specs pass and scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,104 +1,25 @@
-function roman_translator (number) {
- var remainder = 0;
-    while(number > 0) {
+function triangulator (num1, num2, num3) {
 
-        var final = [];
-        var split_number = number.toString().split('');
-        var rev_num = split_number.reverse();
+    var nums = [num1, num2, num3];
+    nums.sort(function(a, b){return b-a});
 
-        if (rev_num[0] <= 3) {
-            var total = rev_num[0] / 1;
-            final[0]= "I".repeat(total);
-            remainder = number % 1;
-            number = remainder;
+    var result = "";
+
+     if (nums[0] <= nums[1] + nums[2]) {
+
+        if ((nums[0] === nums[1]) && (nums[1] === nums[2])) {
+        result = "equilateral";
         }
 
-        else if (rev_num[0] == 4) {
-            final[0] = "IV";
-            remainder = rev_num[0] % 4;
-            number = remainder;
+        if ((nums[1] === nums[2]) && (nums[0] != nums[1])) {
+            result = "isoscele";
         }
-        else if (rev_num[0] >= 5 && rev_num[0] < 9) {
-            var total = rev_num[0] % 5;
-            final[0]= "V" + "I".repeat(total);
-            remainder = number % 1;
-            number = remainder;
-
-        } else if (rev_num[0] == 9) {
-            final[0] = 'IX';
-            remainder = number % 9;
-            number = remainder;
-        }
-
-        if (rev_num[1] <= 3) {
-            var total = rev_num[1] / 1;
-            final[1]= "X".repeat(total);
-            remainder = number % 1;
-            number = remainder;
-        } else if (rev_num[1] == 4) {
-            final[1] = "XL";
-            remainder = rev_num[1] % 4;
-            number = remainder;
-        } else if (rev_num[1] >= 5 && rev_num[1] < 9) {
-            var total = rev_num[1] % 5;
-            final[1] = 'L' + 'X'.repeat(total);
-            remainder = number % 1;
-            number = remainder;
-        } else if (rev_num[1] == 9) {
-            final[1] = 'XC';
-            remainder = number % 9;
-            number = remainder;
-        }
-
-
-        if (rev_num[2] <= 3) {
-            var total = rev_num[2] / 1;
-            final[2]= "C".repeat(total);
-            remainder = number % 1;
-            number = remainder;
-        } else if (rev_num[2] == 4) {
-            final[2] = "CD";
-            remainder = rev_num[2] % 4;
-            number = remainder;
-        } else if (rev_num[2] >= 5 && rev_num[2] < 9) {
-            var total = rev_num[2] % 5;
-            final[2] = 'D' + 'C'.repeat(total);
-            remainder = number % 1;
-            number = remainder;
-        } else if (rev_num[2] == 9) {
-            final[2] = 'CM';
-            remainder = number % 9;
-            number = remainder;
-        }
-
-
-        if (rev_num[3] <= 3) {
-            var total = rev_num[3] / 1;
-            final[3]= "M".repeat(total);
-            remainder = number % 1;
-            number = remainder;
-        }
-
     }
 
-        var very_final = final.reverse().join('');
-        return very_final;
+    else if (nums[0] > nums[1] + nums[2]) {
+        result = "not a triangle";
+    }
+
+    return result;
 
 }
-
-
-        $(document).ready(function() {
-
-            $('#translator').submit(function(event) {
-
-        var number = $('input#number').val();
-
-        var result = roman_translator(number);
-
-        $('.roman').text(result);
-
-        $('#result').show();
-
-        event.preventDefault();
-    });
-});
--- spec/specs.js
+++ spec/specs.js
@@ -1,38 +1,14 @@
-describe('roman_translator', function() {
-    it("returns the symbol of the number 1", function() {
-        expect(roman_translator(1)).to.equal("I");
+describe('triangulator', function() {
+    it("check if the lengths combined could form a triangle in that case : lengths = 2-2-8", function() {
+        expect(triangulator(2, 2, 8)).to.equal("not a triangle");
     });
-    it("returns the symbol of the number 4", function() {
-        expect(roman_translator(4)).to.equal("IV");
+
+    it("check if the lengths combined could form a triangle in that case : lengths = 2-2-2", function() {
+        expect(triangulator(2, 2, 2)).to.equal("equilateral");
     });
-    it("returns the symbol of the number 6", function() {
-        expect(roman_translator(6)).to.equal("VI");
+
+    it("check if the lengths combined could form a triangle in that case : lengths = 2-2-3", function() {
+        expect(triangulator(2, 2, 3)).to.equal("isoscele");
     });
-    it("returns the symbol of the number 8", function() {
-        expect(roman_translator(8)).to.equal("VIII");
-    });
-    it("returns the symbol of the number 9", function() {
-        expect(roman_translator(9)).to.equal("IX");
-    });
-    it("returns the symbol of the number 12", function() {
-        expect(roman_translator(12)).to.equal("XII");
-    });
-    it("returns the symbol of the number 49", function() {
-        expect(roman_translator(49)).to.equal("XLIX");
-    });
-    it("returns the symbol of the number 50", function() {
-        expect(roman_translator(50)).to.equal("L");
-    });
-    it("returns the symbol of the number 96", function() {
-        expect(roman_translator(96)).to.equal("XCVI");
-    });
-    it("returns the symbol of the number 381", function() {
-        expect(roman_translator(381)).to.equal("CCCLXXXI");
-    });
-    it("returns the symbol of the number 900", function() {
-        expect(roman_translator(900)).to.equal("CM");
-    });
-    it("returns the symbol of the number 3999", function() {
-        expect(roman_translator(3999)).to.equal("MMMCMXCIX");
-    });
+    
 });
