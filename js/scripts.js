580159a07d4ecb27a15c69ea0c8ded5d49ff1c6d
virginie <virginietrubiano@gmail.com>
2015-04-10 10:32:07 -0700

    triangulator.html complete and working

--- js/scripts.js
+++ js/scripts.js
@@ -27,3 +27,23 @@ function triangulator (num1, num2, num3) {
     return result;
 
 }
+
+
+$(document).ready(function() {
+
+    $('#form').submit(function(event) {
+
+        var num1 = $('input#num1').val();
+        var num2 = $('input#num2').val();
+        var num3 = $('input#num3').val();
+
+        var result = triangulator(num1, num2, num3);
+
+        $('.result').text(result);
+
+        $('#result').show();
+        event.preventDefault();
+
+    });
+
+});
